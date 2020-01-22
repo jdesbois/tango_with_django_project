0bfebd974620f96a722c06f8c78cb1e0fd071e12
John Desbois <jdesbois@me.com>
2020-01-22 20:45:26 +0000

    Completed Chapter 4: Tests passed

--- rango/views.py
+++ rango/views.py
@@ -4,11 +4,12 @@ from django.http import HttpResponse
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': 'Cruncy, creamy, cookie, candy, cupcake!'}
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     # Return a rendered responce to send to the client.
     # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use. 
+    # Note that the first parameter is the template we wish to use.
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    context_dict = {'boldmessage' : 'John'}
+    return render(request, 'rango/about.html', context=context_dict)
